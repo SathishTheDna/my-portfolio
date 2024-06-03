129290205875c08a8770887f7404b2202920a109
sathish-mountvisual <sathish@mountvisual.no>
2024-06-03 20:40:28 +0200

    Modified the App Footer component.

--- src/components/shared/AppFooter.jsx
+++ src/components/shared/AppFooter.jsx
@@ -1,38 +1,27 @@
 import {
 	FiGithub,
-	FiTwitter,
 	FiLinkedin,
-	FiGlobe,
-	FiYoutube,
+	FiMail
 } from 'react-icons/fi';
 import AppFooterCopyright from './AppFooterCopyright';
 
 const socialLinks = [
 	{
 		id: 1,
-		icon: <FiGlobe />,
-		url: 'https://www.stoman.me/',
+		icon: <FiLinkedin />,
+		url: 'https://www.linkedin.com/in/sathish-raja-bommannan/',
 	},
 	{
 		id: 2,
 		icon: <FiGithub />,
-		url: 'https://github.com/realstoman',
+		url: 'https://github.com/SathishTheDna',
 	},
 	{
 		id: 3,
-		icon: <FiTwitter />,
-		url: 'https://twitter.com/realstoman',
+		icon: <FiMail />,
+		url: 'mailto:sathishrajabommannan@gmail.com',
 	},
-	{
-		id: 4,
-		icon: <FiLinkedin />,
-		url: 'https://www.linkedin.com/in/realstoman',
-	},
-	{
-		id: 5,
-		icon: <FiYoutube />,
-		url: 'https://www.youtube.com/c/realstoman',
-	},
+	
 ];
 
 const AppFooter = () => {
