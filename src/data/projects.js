df1878d4353cf4729fda5a73c6797e96d0882be6
sathish-mountvisual <sathish@mountvisual.no>
2024-06-04 01:55:56 +0200

    Updated the list of all projects.

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,13 +1,155 @@
-// Import images
-import WebImage1 from '../images/web-project-1.jpg';
-import WebImage2 from '../images/web-project-2.jpg';
-import MobileImage1 from '../images/mobile-project-1.jpg';
-import MobileImage2 from '../images/mobile-project-2.jpg';
-import UIImage1 from '../images/ui-project-1.jpg';
-import UIImage2 from '../images/ui-project-2.jpg';
+import ARDigitalTwin from '../images/projects/ar-digital-twin/ar-digital-twin-3.png';
+import ARPongalGreetings from '../images/projects/ar-pongal-greetings/pongal-1.png';
+import BrowserInUnity from '../images/projects/browser-in-unity/vuplex-brower.png';
+import Drone from '../images/projects/emergency-rescue-drone/drone-3.png';
+import GloveGesture from '../images/projects/glove-gesture-recognition/glove-gesture-2.png';
+import HandsOnlyLocomotion from '../images/projects/hands-only-locomotion-system/locomotion-1.png';
+import HullCleaner from '../images/projects/hull-cleaner/hull-cleaner-1.png';
+import Trollheimen from '../images/projects/itrollheimen/itrollheimen-1.png';
+import KongsbergDisplay from '../images/projects/kongsberg-product-display/kongsberg-1.jpeg';
+import MagiPaint from '../images/projects/magipaint-ar/magipaint-2.png';
+import MagiStack from '../images/projects/magistack-ar/stack-3.png';
+import ManomotionInternship from '../images/projects/manomotion-internship-demo/manomotion-internship-1.png';
+import ManomotionAddOn from '../images/projects/manomotion-sdk-add-on-plugins/manomotion-addon.png';
+import SalmonFarm from '../images/projects/salmon-farm-showcase/salmon-1.png';
+import StoryBits from '../images/projects/storybits/storybits.png';
+import TuioClient from '../images/projects/tuio-client-package/tuio-client-package.png';
+import VRArchitecture from '../images/projects/vr-architecture-visualization/vr-architecture-1.png';
+import VRDevKit from '../images/projects/vr-dev-toolkit/vr-dev-toolkit.png';
+import VRHandKeypad from '../images/projects/vr-hand-keypad/vr-hand-keypad.png';
+import VRShipSimulator from '../images/projects/vr-ship-bridge-simulator/vr-ship-1.png';
+import XRPresentations from '../images/projects/xr-presentations/xr-presentations-1.png';
 
 export const projectsData = [
 	{
+		id: 1,
+		title: 'XR Presentation Rooms',
+		category: 'Unity VR Application',
+		img: XRPresentations,
+	},
+	{
+		id: 2,
+		title: 'VR Ship Safe Navigation Training',
+		category: 'Unity VR Application',
+		img: VRShipSimulator,
+	},
+	{
+		id: 3,
+		title: 'iTrollheimen AR',
+		category: 'Unity AR Application',
+		img: Trollheimen,
+	},
+	{
+		id: 4,
+		title: 'Hands-only Locomotion System for Oculus Quest',
+		category: 'Unity VR Application',
+		img: HandsOnlyLocomotion,
+	},
+	{
+		id: 5,
+		title: 'VR Hand Keypad',
+		category: 'Unity VR Application',
+		img: VRHandKeypad,
+	},
+	{
+		id: 6,
+		title: 'MagiStack AR',
+		category: 'Unity AR Application',
+		img: MagiStack,
+	},
+	{
+		id: 7,
+		title: 'MagiPaint AR',
+		category: 'Unity AR Application',
+		img: MagiPaint,
+	},
+	{
+		id: 8,
+		title: 'Emergency Search and Rescue Hexacopter',
+		category: 'IoT Project',
+		img: Drone,
+	},
+	{
+		id: 9,
+		title: 'Salmon Farm Showcase App',
+		category: 'Unity Non-XR Application',
+		img: SalmonFarm,
+	},
+	{
+		id: 10,
+		title: 'VR Architecture Visualization App',
+		category: 'Unity VR Application',
+		img: VRArchitecture,
+	},
+	{
+		id: 11,
+		title: 'Multi-touch supported Product showcase App with Object Recognition',
+		category: 'Unity Non-XR Application',
+		img: KongsbergDisplay,
+	},
+	{
+		id: 12,
+		title: 'Real-time Numerical Gesture Recognition using MPU 9250 Motion Sensor',
+		category: 'Unity Non-XR Application',
+		img: GloveGesture,
+	},
+	{
+		id: 13,
+		title: 'VR Development Toolkit',
+		category: 'Unity VR Application',
+		img: VRDevKit,
+	},
+	{
+		id: 14,
+		title: 'Unity Tuio Client and TouchScript Integration',
+		category: 'Unity Non-XR Application',
+		img: TuioClient,
+	},
+	{
+		id: 15,
+		title: 'Storybits',
+		category: 'Unity AR Application',
+		img: StoryBits,
+	},
+	{
+		id: 16,
+		title: 'Manomotion SDK Add-On Plugins',
+		category: 'Unity AR Application',
+		img: ManomotionAddOn,
+	},
+	{
+		id: 17,
+		title: 'Manomotion SDK Showcase App',
+		category: 'Unity AR Application',
+		img: ManomotionInternship,
+	},
+	{
+		id: 18,
+		title: 'Hull Cleaner Robot Simulator',
+		category: 'Unity Non-XR Application',
+		img: HullCleaner,
+	},
+	{
+		id: 19,
+		title: 'Multi-touch supported browsers in Unity',
+		category: 'Unity Non-XR Application',
+		img: BrowserInUnity,
+	},
+	{
+		id: 20,
+		title: 'AR Pongal Greetings',
+		category: 'Unity AR Application',
+		img: ARPongalGreetings,
+	},
+	{
+		id: 21,
+		title: 'Real-time Industrial Equipment Visualization in AR',
+		category: 'Unity AR Application',
+		img: ARDigitalTwin,
+	}	
+];
+
+/* {
 		id: 1,
 		title: 'Google Health Platform',
 		category: 'Web Application',
@@ -17,35 +159,4 @@ export const projectsData = [
 			publishDate: 'Jul 26, 2021',
 			tags: 'UI / Frontend',
 		},
-	},
-	{
-		id: 2,
-		title: 'Phoenix Digital Agency',
-		category: 'Mobile Application',
-		img: MobileImage2,
-	},
-	{
-		id: 3,
-		title: 'Project Management UI',
-		category: 'UI/UX Design',
-		img: UIImage1,
-	},
-	{
-		id: 4,
-		title: 'Cloud Storage Platform',
-		category: 'UI/UX Design',
-		img: UIImage2,
-	},
-	{
-		id: 5,
-		title: 'React Social App',
-		category: 'Mobile Application',
-		img: MobileImage1,
-	},
-	{
-		id: 6,
-		title: 'Apple Design System',
-		category: 'Web Application',
-		img: WebImage1,
-	},
-];
+	}, */
