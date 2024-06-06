3fad332cb20911816f0853f106dfce1d610de8a2
sathish-mountvisual <sathish@mountvisual.no>
2024-06-07 00:29:46 +0200

    Updated the SingleProjectContext to fetch appropriate data based on the projectId in the URL. When the projectData with projectId not found, navigating to home page.

--- src/context/SingleProjectContext.jsx
+++ src/context/SingleProjectContext.jsx
@@ -1,12 +1,26 @@
-import { useState, createContext } from 'react';
-import { singleProjectData as singleProjectDataJson } from '../data/singleProjectData';
+import { useParams, useNavigate } from 'react-router-dom';
+import { useState, useEffect, createContext } from 'react';
+import { projectsData as projectsDataJson } from '../data/projectsDatas';
 
 const SingleProjectContext = createContext();
 
 export const SingleProjectProvider = ({ children }) => {
-	const [singleProjectData, setSingleProjectData] = useState(
-		singleProjectDataJson
-	);
+	const {projectId} = useParams();
+	const navigate = useNavigate();
+	const [singleProjectData, setSingleProjectData] = useState(projectsDataJson[0]);
+
+	useEffect(() => {
+		const projectData = projectsDataJson.find(
+			project => project.ProjectId.toString() === projectId
+		);
+
+		setSingleProjectData(projectData);
+	}, [projectId]);
+
+	if (!singleProjectData) {
+		navigate('/');
+		return null;
+	}
 
 	return (
 		<SingleProjectContext.Provider
