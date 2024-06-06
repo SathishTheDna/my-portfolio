56dfcea65380d1c434fe5543a1fdb16f77f88fc4
sathish-mountvisual <sathish@mountvisual.no>
2024-06-06 19:55:00 +0200

    Add Manomotion plugins project data.

---
+++ src/data/projectsData/manomotion-addon-plugins-data.js
@@ -0,0 +1,45 @@
+import manomotionAddon from '../../images/projects/manomotion-sdk-add-on-plugins/manomotion-addon.png';
+
+export const manomotionAddonData = {
+	ProjectId: 'manomotion-addon-plugins',
+	ProjectHeader: {
+		title: 'Manomotion SDK add-on plugins',
+		timeline: 'May 2019 - Sep 2019',
+		tags: 'Unity, AR'
+	},
+	ProjectMedia: [
+		{
+			id: 1,
+			type: 'image',
+			title: 'Manomotion Add-on Plugins',
+			img: manomotionAddon
+		}
+	],
+	ProjectInfo: {
+		Technologies: [
+			{
+				title: 'Tools & Technologies',
+				techs: [
+                    'Unity', 'Manomotion SDK', 'ARFoundation', 'Wikitude', 'Vuforia'
+				],
+			},
+		],
+		ObjectivesHeading: 'Overview',
+		ObjectivesDetails: 'Developed Manomotion add-on plugins for ARFoundation, Wikitude, and Vuforia SDKs.',
+		ProjectDetailsHeading: 'Project Details',
+		ProjectDetails: [
+			{
+				id: 1,
+				details:
+                    `During my internship at Manomotion, I contributed to the development of Vuforia, Wikitude, and ARFoundation add-on plugins for the Manomotion SDK. The Manomotion SDK enables hand tracking and gesture recognition in AR and VR applications, addressing the challenge of accurately detecting and interpreting hand movements and gestures in real-time. By enhancing user interaction and immersion in AR and VR experiences, the Manomotion SDK plays a crucial role in advancing the capabilities of interactive digital environments.`    
+            },
+			{
+				id: 2,
+				details:
+                    `My contribution to the development of add-on plugins aimed to extend the functionality of the Manomotion SDK, enabling seamless integration with popular AR platforms and empowering developers to create more immersive and interactive AR applications. I collaborated with the community manager at Manomotion aimed to enhance hand tracking and gesture recognition capabilities in AR and VR applications, facilitating smoother development workflows and accelerating the adoption of advanced interaction features in AR experiences.`
+            }
+		],
+		ExternalLinksHeading: 'Useful Links',
+		ExternalLinks: []
+	}
+}
