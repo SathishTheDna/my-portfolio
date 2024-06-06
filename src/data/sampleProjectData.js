29c311740bd9d648b620ccdda2f91e4926b2fc1e
sathish-mountvisual <sathish@mountvisual.no>
2024-06-06 16:44:40 +0200

    Created a SampleProjectData for reference. Removed unused data fields in the JSON data.

---
+++ src/data/sampleProjectData.js
@@ -0,0 +1,67 @@
+const sampleProjectData = {
+	ProjectHeader: {
+		title: 'Sample Project',
+		timeline: 'Jan 2023 - Dec 2023',
+		tags: 'Unity, AR, VR, IoT, React, ASP.NET'
+	},
+	ProjectMedia: [
+		{
+			id: 1,
+			type: 'image',
+			title: 'Sample Image',
+			img: 'someURL'
+		},
+		{
+			id: 2,
+			type: 'video',
+			title: 'Sample Video',
+			url: 'someURL'
+		}
+	],
+	ProjectInfo: {
+		Summary: 'Sample Project in React',
+		Technologies: [
+			{
+				title: 'Tools & Technologies',
+				techs: [
+                    'React',
+                    'HTML',
+                    'CSS'
+				],
+			},
+		],
+		ObjectivesHeading: 'Overview',
+		ObjectiveDetails: 'Sample Project in React for Portfolio',
+		ProjectDetailsHeading: 'Details',
+		ProjectDetails: [
+			{
+				id: 1,
+				details:
+					'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.',
+			},
+			{
+				id: 2,
+				details:
+					'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?',
+			},
+			{
+				id: 3,
+				details:
+					'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?',
+			},
+			{
+				id: 4,
+				details:
+					'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.',
+			},
+		],
+		ExternalLinksHeading: 'Useful Links',
+		ExternalLinks: [
+			{
+				id: 1,
+				name: 'Project Documents',
+				url: 'someURL'
+			}
+		]
+	}
+}
