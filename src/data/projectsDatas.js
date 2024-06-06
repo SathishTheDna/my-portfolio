f33c1064434266fde819bf6469a3616118f70a9d
sathish-mountvisual <sathish@mountvisual.no>
2024-06-07 00:28:00 +0200

    Created projectsDatas.js to fetch all the project datas as one single array, projectsData.

---
+++ src/data/projectsDatas.js
@@ -0,0 +1,45 @@
+import { arDigitalTwinData } from "./projectsData/ar-digital-twin-data";
+import { arPongalGreetingsData } from "./projectsData/ar-pongal-greeting-data";
+import { emergencySearchDroneData } from "./projectsData/esr-drone-data";
+import { gestureGloveData } from "./projectsData/gesture-glove-data";
+import { hullCleanerData } from "./projectsData/hull-cleaner-data";
+import { itrollheimenData } from "./projectsData/itrollheimen-data";
+import { magipaintData } from "./projectsData/magipaint-ar-data";
+import { magistackData } from "./projectsData/magistack-ar-data";
+import { manomotionAddonData } from "./projectsData/manomotion-addon-plugins-data";
+import { manomotionShowcaseData } from "./projectsData/manomotion-sdk-showcase-data";
+import { multiTouchBrowsersData } from "./projectsData/multi-touch-browsers-data";
+import { multiTouchProductShowcaseData } from "./projectsData/multi-touch-products-data";
+import { salmonFarmShowcaseData } from "./projectsData/salmon-farm-data";
+import { storybitsData } from "./projectsData/storybits-data";
+import { tuioClientPackageData } from "./projectsData/tuio-client-package-data";
+import { vrArchVizData } from "./projectsData/vr-arch-visualization-data";
+import { vrDevToolkitData } from "./projectsData/vr-dev-toolkit-data";
+import { vrHandKeypadData } from "./projectsData/vr-hand-keypad-data";
+import { vrLocomotionData } from "./projectsData/vr-locomotion-data";
+import { vrShipSimulatorData } from "./projectsData/vr-ship-simulator-data";
+import { xrPresentationsData } from "./projectsData/xr-presentation-rooms-data";
+
+export const projectsData = [
+    xrPresentationsData,
+    vrShipSimulatorData,
+    itrollheimenData,
+    vrLocomotionData,
+    vrHandKeypadData,
+    magistackData,
+    magipaintData,
+    emergencySearchDroneData,
+    salmonFarmShowcaseData,
+    vrArchVizData,
+    multiTouchProductShowcaseData,
+    gestureGloveData,
+    vrDevToolkitData,
+    tuioClientPackageData,
+    storybitsData,
+    manomotionAddonData,
+    manomotionShowcaseData,
+    hullCleanerData,
+    multiTouchBrowsersData,
+    arPongalGreetingsData,
+    arDigitalTwinData
+];
