dab7befc03e56a4f127eed094bfa268fdfb4062d
sathish-mountvisual <sathish@mountvisual.no>
2024-06-06 17:18:45 +0200

    Modified ProjectGallery component to support inline youtube videos along with the imported images.

--- src/components/projects/ProjectGallery.jsx
+++ src/components/projects/ProjectGallery.jsx
@@ -6,15 +6,28 @@ const ProjectGallery = () => {
 
 	return (
 		<div className="grid grid-cols-1 sm:grid-cols-3 sm:gap-10 mt-12">
-			{singleProjectData.ProjectMedia.map((project) => {
+			{singleProjectData.ProjectMedia.map((media) => {
 				return (
-					<div className="mb-10 sm:mb-0" key={project.id}>
-						<img
-							src={project.img}
-							className="rounded-xl cursor-pointer shadow-lg sm:shadow-none"
-							alt={project.title}
-							key={project.id}
-						/>
+					<div className="mb-10 sm:mb-0" key={media.id}>
+						{
+							media.type === 'video' ?
+								<iframe
+									width="100%"
+									height="100%"
+									src={`https://www.youtube.com/embed/${media.url.split('v=')[1]}`}
+									title={media.title}
+									allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+									className="rounded-xl cursor-pointer shadow-lg sm:shadow-none"
+									allowFullScreen
+								></iframe>
+							:
+							<img
+								src={media.img}
+								className="rounded-xl cursor-pointer shadow-lg sm:shadow-none"
+								alt={media.title}
+								key={media.id}
+							/>
+						}						
 					</div>
 				);
 			})}
