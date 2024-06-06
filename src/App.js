c6e83526525b3a1cd7b9aa4fd57505ca1e9d522c
sathish-mountvisual <sathish@mountvisual.no>
2024-06-07 00:28:46 +0200

    Made sure any unknown route path will be redirected to Home page.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { AnimatePresence } from 'framer-motion';
 import { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop';
 import AppFooter from './components/shared/AppFooter';
 import AppHeader from './components/shared/AppHeader';
@@ -32,6 +32,7 @@ function App() {
 
 							<Route path="about" element={<About />} />
 							<Route path="contact" element={<Contact />} />
+							<Route path="*" element={<Navigate to="/" />} />
 						</Routes>
 					</Suspense>
 					<AppFooter />
