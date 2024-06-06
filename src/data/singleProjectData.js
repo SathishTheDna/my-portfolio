88dff1ebc078ca96270674e087813e372f08d384
sathish-mountvisual <sathish@mountvisual.no>
2024-06-06 16:43:08 +0200

    Added External Links Data in Single Project Data.

--- src/data/singleProjectData.js
+++ src/data/singleProjectData.js
@@ -100,6 +100,19 @@ export const singleProjectData = {
 					'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea.',
 			},
 		],
+		ExternalLinksHeading: 'Useful Links',
+		ExternalLinks: [
+			{
+				id: 1,
+				name: 'Project Documents',
+				url: 'https://www.linkedin.com/in/sathish-raja-bommannan/'
+			},
+			{
+				id: 2,
+				name: 'Project Documents',
+				url: 'https://www.linkedin.com/in/sathish-raja-bommannan/'
+			},
+		],	
 		SocialSharingHeading: 'Share This',
 		SocialSharing: [
 			{
