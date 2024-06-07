c2ff0cf2af68e5347928f1b214a613519574cd66
sathish-mountvisual <sathish@mountvisual.no>
2024-06-07 10:37:00 +0200

    Modified the copyright footer text.

--- src/components/shared/AppFooterCopyright.jsx
+++ src/components/shared/AppFooterCopyright.jsx
@@ -3,14 +3,14 @@ function AppFooterCopyright() {
 		<div className="font-general-regular flex justify-center items-center text-center">
 			<div className="text-lg text-ternary-dark dark:text-ternary-light">
 				Copyright &copy; {new Date().getFullYear()}
-				<a
+				{/* <a
 					href="https://github.com/SathishTheDna/my-portfolio"
 					target="__blank"
 					className="hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
 				>
 					Portfolio 
 				</a>
-				&nbsp;of
+				&nbsp;of */}
 				<a
 					href="https://www.linkedin.com/in/sathish-raja-bommannan/"
 					target="__blank"
